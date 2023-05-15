3d20cfad443b865fb11e5b1468e9c61022c54537
10mematii <133607424+10mematii@users.noreply.github.com>
2023-05-15 15:30:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
+//Kelime ters çevirme
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 
-// index.js dosyası
+//Random sayı
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//İlk harfi büyük harf yapma
+function capitalizeString(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 // Örnek bir fonksiyon
 function greet(name) {
   return `Merhaba, ${name}!`;
 }
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+
+function divide(a, b) {
+  return a / b;
+}
 
 // Modülümüzün exports objesine fonksiyonu ekliyoruz
 module.exports = {
-  greet
+  greet,
+  add,
+  subtract,
+  multiply,
+  divide,
+  generateRandomNumber,
+  capitalizeString,
+  reverseString
 };
