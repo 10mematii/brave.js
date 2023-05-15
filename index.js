3c9a4e56d2b1c4785085e32e828fb44faf7f9e8a
10mematii <133607424+10mematii@users.noreply.github.com>
2023-05-15 14:00:21 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// index.js dosyası
+
+// Örnek bir fonksiyon
+function greet(name) {
+  return `Merhaba, ${name}!`;
+}
+
+// Modülümüzün exports objesine fonksiyonu ekliyoruz
+module.exports = {
+  greet
+};
